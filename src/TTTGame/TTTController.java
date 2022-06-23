e8d050180ef7c00c5eb303a460e9e347aa38e687
luigi <luigi.dimicco.it@gmai.com>
2022-06-23 17:52:41 +0200

    Initial commit

---
+++ src/TTTGame/TTTController.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TTTGame;
+
+import java.beans.*;
+import java.io.Serializable;
+import javax.swing.JLabel;
+
+/**
+ *
+ * @author luigi
+ */
+public class TTTController extends JLabel implements VetoableChangeListener {
+    
+   
+    private TTCell.TTTState lastMove;
+    
+    public TTTController() {
+        super();
+        super.setText("START GAME");
+        lastMove = null;
+//        propertySupport = new PropertyChangeSupport(this);
+    }
+    
+
+    @Override
+    public void vetoableChange(PropertyChangeEvent pce) throws PropertyVetoException {
+        System.out.println("Vetoable change! "+ pce.getPropertyName() + " "
+        + pce.getNewValue());
+        TTCell.TTTState old = (TTCell.TTTState) pce.getOldValue();
+        TTCell.TTTState newV = (TTCell.TTTState) pce.getNewValue();
+        System.out.println("lastMove: "+lastMove + " Cell "+pce.getSource().hashCode()+
+                ":\n old value: "+old +", new value:"+newV);
+        if(old != TTCell.TTTState.INITIAL || newV == lastMove)
+            throw new PropertyVetoException("Not "+newV+" turn!", pce);
+       
+        lastMove = newV;
+        String turn = newV.toString() == "X"? "O" : "X";
+        setText("Next move: "+ turn);
+    }
+    
+}
